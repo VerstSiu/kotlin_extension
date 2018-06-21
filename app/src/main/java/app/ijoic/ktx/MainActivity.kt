d25c94705c6fc74cc9ac7560905081ed589a70d3
Verst Siu <verstsiu@126.com>
2018-06-21 22:48:51 +0800

    :sparkles: Add rxjava extension api

--- app/src/main/java/app/ijoic/ktx/MainActivity.kt
+++ app/src/main/java/app/ijoic/ktx/MainActivity.kt
@@ -1,12 +1,17 @@
 package app.ijoic.ktx
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.ijoic.ktx.rxjava.execute
+import com.ijoic.ktx.util.AsyncLooper
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    AsyncLooper.execute({ "..." }, { Toast.makeText(this@MainActivity, it, Toast.LENGTH_SHORT).show() })
   }
 }
---
+++ ktx-rxjava/src/main/java/com/ijoic/ktx/rxjava/_AsyncLooper.kt
@@ -0,0 +1,66 @@
+/*
+ *
+ *  Copyright(c) 2018 VerstSiu
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package com.ijoic.ktx.rxjava
+
+import com.ijoic.ktx.util.AsyncLooper
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+
+/**
+ * Execute expected action.
+ *
+ * @param scheduler scheduler.
+ * @param action action.
+ * @return execute disposable.
+ */
+fun AsyncLooper.execute(scheduler: Scheduler, action: () -> Unit): Disposable {
+  return scheduler.scheduleDirect(action)
+}
+
+/**
+ * Execute fromAction and deliver result to toAction.
+ *
+ * @param fromAction from action.
+ * @param toAction to action.
+ * @param from from scheduler.
+ * @param to to scheduler.
+ */
+fun<T> AsyncLooper.execute(fromAction: () -> T, toAction: (T) -> Unit, from: Scheduler = Schedulers.io(), to: Scheduler = AndroidSchedulers.mainThread()) {
+  from.scheduleDirect {
+    val result = fromAction.invoke()
+    to.scheduleDirect { toAction.invoke(result) }
+  }
+}
+
+/**
+ * Execute expected action on io thread.
+ *
+ * @param action action.
+ * @return execute disposable.
+ */
+fun AsyncLooper.io(action: () -> Unit) = execute(Schedulers.io(), action)
+
+/**
+ * Execute expected action on computation thread.
+ *
+ * @param action action.
+ * @return execute disposable.
+ */
+fun AsyncLooper.compute(action: () -> Unit) = execute(Schedulers.computation(), action)
